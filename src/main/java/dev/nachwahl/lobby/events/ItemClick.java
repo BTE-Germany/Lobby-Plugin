259e5bb43b0061c4404fcb5e447d5ebbce18cf22
RaptusBube <74785642+RaptusBube@users.noreply.github.com>
2023-06-25 02:34:40 +0200

    Lobby Heads Advertising

--- src/main/java/dev/nachwahl/lobby/events/ItemClick.java
+++ src/main/java/dev/nachwahl/lobby/events/ItemClick.java
@@ -4,15 +4,26 @@ import dev.nachwahl.lobby.Lobby;
 import dev.nachwahl.lobby.utils.guis.AccountGUI;
 import dev.nachwahl.lobby.utils.guis.NavigatorGUI;
 import lombok.SneakyThrows;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.util.Vector;
+
+import java.awt.*;
+import java.awt.TextComponent;
+import java.util.Objects;
 
 public class ItemClick implements Listener {
 
@@ -27,6 +38,8 @@ public class ItemClick implements Listener {
     public void onItemClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if(event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
+
+
             this.lobby.getLanguageAPI().getLanguage(player, language -> {
                 if(event.getItem() != null) {
                     if(event.getItem().getItemMeta().displayName().equals(this.lobby.getLanguageAPI().getMessage(language, "navigator.itemName"))) {
@@ -37,7 +50,51 @@ public class ItemClick implements Listener {
                     }
                 }
             });
+
         }
+        if((event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_BLOCK) && Objects.equals(event.getHand(), EquipmentSlot.HAND)){
+
+            Block block = event.getClickedBlock();
+            Location youtube = new Location(block.getWorld(),-5, 178, 10);
+            Location twitch = new Location(block.getWorld(),-3,178,9);
+            Location tiktok = new Location(block.getWorld(), 0, 178, 6);
+            Location instagram = new Location(block.getWorld(), 1, 178, 4);
+            Location website = new Location(block.getWorld(), -14, 178, 9);
+            Location discord = new Location(block.getWorld(), -18, 178, 5);
+            Location location = block.getLocation();
+
+            this.lobby.getLanguageAPI().getLanguage(player, language -> {
+
+                net.kyori.adventure.text.Component textComponent = null;
+
+                String url = "";
+                if (location.equals(youtube)) {
+                    url = "https://www.youtube.com/@BTEGermany";
+                    textComponent = this.lobby.getLanguageAPI().getMessage(language, "advertisement.checkOut", Placeholder.parsed("platform", "<red>YouTube</red>"));
+                } else if (location.equals(twitch)) {
+                    url = "https://www.twitch.tv/btegermany";
+                    textComponent = this.lobby.getLanguageAPI().getMessage(language, "advertisement.checkOut", Placeholder.parsed("platform", "<dark_purple>Twitch</dark_purple>"));
+                } else if (location.equals(tiktok)) {
+                    url = "https://www.tiktok.com/@btegermany";
+                    textComponent = this.lobby.getLanguageAPI().getMessage(language, "advertisement.checkOut", Placeholder.parsed("platform", "<black>TikTok</black>"));
+                } else if (location.equals(instagram)) {
+                    url = "https://www.instagram.com/btegermany/";
+                    textComponent = this.lobby.getLanguageAPI().getMessage(language, "advertisement.checkOut", Placeholder.parsed("platform", "<red>Insta</red><light_purple>gram</light_purple>"));
+                } else if (location.equals(website)) {
+                    url = "https://buildtheearth.net/";
+                    textComponent = this.lobby.getLanguageAPI().getMessage(language, "advertisement.checkOut", Placeholder.parsed("platform", "<blue>BuildTheEarth.net Website</blue>"));
+                } else if (location.equals(discord)) {
+                    url = "https://discord.gg/GkSxGTYaAJ";
+                    textComponent = this.lobby.getLanguageAPI().getMessage(language, "advertisement.checkOut", Placeholder.parsed("platform", "<gray>Discord</gray>"));
+                } else {
+                    return;
+                }
+                net.kyori.adventure.text.Component hoverEventComponent = textComponent.hoverEvent(this.lobby.getLanguageAPI().getMessage(language, "advertisement.openURL", Placeholder.parsed("url", url)));
+                player.sendMessage(hoverEventComponent.clickEvent(ClickEvent.openUrl(url)));
+            });
+
+        }
+
         if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
             if(player.getPassengers().size()>=1) {
                 Player passenger = (Player)  player.getPassengers().get(0);
