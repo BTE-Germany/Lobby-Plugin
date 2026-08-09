6753b5ded7d8a08e9d9870494bb92cb8e0802799
Zoriot <zoriot@proton.me>
2026-08-09 22:32:07 +0200

    fix: Sync player.hidePlayer
    Fixes DEV-0171. Fixes Sentry 8218dc5cd524410daf750186c10d6d38.

--- src/main/java/dev/nachwahl/lobby/utils/Actions.java
+++ src/main/java/dev/nachwahl/lobby/utils/Actions.java
@@ -1,15 +1,15 @@
 package dev.nachwahl.lobby.utils;
 
 import dev.nachwahl.lobby.LobbyPlugin;
+import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+@UtilityClass
 public class Actions {
-    private Actions() {
-    }
 
     public static void performJoinActions(@NotNull LobbyPlugin lobbyPlugin, Player player) {
         lobbyPlugin.getHotbarItems().setHotbarItems(player);
@@ -21,17 +21,20 @@ public class Actions {
         player.setAllowFlight(false); // No double jump
         player.setGameMode(GameMode.ADVENTURE);
 
-        lobbyPlugin.getUserSettingsAPI().getBooleanSetting(player, "realTime", (s) -> {
-            if (s) {
+        lobbyPlugin.getUserSettingsAPI().getBooleanSetting(player, "realTime", s -> {
+            if (Boolean.TRUE.equals(s)) {
                 player.setPlayerTime(lobbyPlugin.getRealTime().getTime(), false);
             } else {
                 player.resetPlayerTime();
             }
         });
 
-        lobbyPlugin.getUserSettingsAPI().getBooleanSetting(player, "playerVisibility", (value) -> {
+        lobbyPlugin.getUserSettingsAPI().getBooleanSetting(player, "playerVisibility", value -> {
             if (Boolean.FALSE.equals(value)) {
-                Bukkit.getOnlinePlayers().forEach(p -> player.hidePlayer(lobbyPlugin, p));
+                Bukkit.getScheduler().runTask(lobbyPlugin, () -> Bukkit.getOnlinePlayers().forEach(p -> {
+                    if (p == player) return;
+                    player.hidePlayer(lobbyPlugin, p);
+                }));
             }
         });
     }
